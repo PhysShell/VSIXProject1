f4f23ab4ce3aa2106dfb1b1dbdc2e583f8494c59
Федор Шелипов <f.shelipov@sector.kz>
2021-05-27 15:34:54 +0600

    Initial commit

---
+++ VSIXProject1/Formatter.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    class Formatter
+    {
+        private readonly Microsoft.VisualStudio.Text.Editor.IWpfTextView view;
+        private bool isChangingText;
+
+        public Formatter(Microsoft.VisualStudio.Text.Editor.IWpfTextView view)
+        {
+            this.view = view;
+            this.view.TextBuffer.Changed += TextBuffer_Changed;
+            this.view.TextBuffer.PostChanged += TextBuffer_PostChanged;
+        }
+
+        private void TextBuffer_Changed(object sender, TextContentChangedEventArgs e)
+        {
+            if (!isChangingText)
+            {
+                isChangingText = true;
+                FormatCode(e);
+            }
+        }
+
+        private void TextBuffer_PostChanged(object sender, EventArgs e)
+        {
+            isChangingText = false;
+        }
+
+        private void FormatCode(TextContentChangedEventArgs e)
+        {
+            if (e.Changes != null)
+            {
+                for (int i = 0; i < e.Changes.Count; i++)
+                {
+                    HandleChange(e.Changes[0].NewText);
+                }
+            }
+        }
+
+        private void HandleChange(string newText)
+        {
+            ITextEdit edit = view.TextBuffer.CreateEdit();
+            //edit.Insert(0, "Hello");
+            //edit.Apply();
+        }
+    }
+}
---
+++ VSIXProject1/TextViewCreationListener.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace Extension
+{
+    [ContentType("code")]
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal sealed class TextViewCreationListener : Microsoft.VisualStudio.Text.Editor.IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            new Formatter(textView);
+        }
+    }
+}
